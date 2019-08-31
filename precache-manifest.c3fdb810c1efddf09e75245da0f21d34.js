9effc50bec09df106ca3560b93eb9e8ac1e19a9e
Indranil Tiwary <indraniltiwary@Indranils-MacBook-Pro.local>
2019-09-01 01:59:52 +0530

    Updates

---
+++ precache-manifest.c3fdb810c1efddf09e75245da0f21d34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c007ec91cefe599d2da9e6e05dcd3fd4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d7b6ddbefcb597f10af3",
+    "url": "/monsters-rolodex/static/css/main.40dab5bd.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "d7b6ddbefcb597f10af3",
+    "url": "/monsters-rolodex/static/js/main.013c89c2.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
